1ecb8848df82fd226dcbb816e4535fd6a6d07e45
demingongo <sdemingongo@gmail.com>
2023-03-08 00:21:31 +0100

    keybindings, taglist_buttons, tasklist_buttons

---
+++ config/awesome/tasklist_buttons.lua
@@ -0,0 +1,24 @@
+local gears = require("gears")
+local awful = require("awful")
+
+return gears.table.join(
+    awful.button({}, 1, function(c)
+        if c == client.focus then
+            c.minimized = true
+        else
+            c:emit_signal(
+                "request::activate",
+                "tasklist",
+                { raise = true }
+            )
+        end
+    end),
+    awful.button({}, 3, function()
+        awful.menu.client_list({ theme = { width = 250 } })
+    end),
+    awful.button({}, 4, function()
+        awful.client.focus.byidx(1)
+    end),
+    awful.button({}, 5, function()
+        awful.client.focus.byidx(-1)
+    end))
