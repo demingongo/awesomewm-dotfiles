6196aed035de6bc86e8df7d1a58d2a1021c93df7
demingongo <sdemingongo@gmail.com>
2023-03-21 00:43:33 +0100

    autostart_shell_cmds

--- config/awesome/my/autostart.lua
+++ config/awesome/my/autostart.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
+local naughty = require("naughty")
 
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
@@ -17,6 +18,23 @@ awful.spawn.once("numlockx on")
 awful.spawn.once('xinput disable "ETPS/2 Elantech Touchpad"')
 -- disable built-in PC speaker (or beeper)
 awful.spawn.once("xset -b")
+
+if type(beautiful.autostart_shell_cmds) == "table" then
+	for _, cmd in ipairs(beautiful.autostart_shell_cmds) do
+		awful.spawn.easy_async_with_shell(
+			cmd,
+			function(_, stderr)
+				if stderr and not stderr == "" then
+					naughty.notify({
+						preset = naughty.config.presets.critical,
+						title = "autostart_shell_cmds error",
+						text = stderr
+					})
+				end
+			end
+		)
+	end
+end
 
 if type(beautiful.autostart) == "function" then
 	beautiful.autostart()
