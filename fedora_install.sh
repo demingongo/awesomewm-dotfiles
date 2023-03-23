833e71129ae5928b699b4c2e72423e106e12afa4
demingongo <sdemingongo@gmail.com>
2023-03-23 10:21:14 +0100

    update scripts

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-### Tested on Fedora 37 Workstation
-###
-### Execute the script as a simple user and NOT as root.
-###
+# fedora_install.sh
+#
+# Description: install awesomewm-dotfiles
+# Author: demingongo
+# Link: https://github.com/demingongo
+# Availability: https://github.com/demingongo/awesomewm-dotfiles
+
+# Tested on Fedora 37 Workstation
+#
+# Execute the script as a simple user and NOT as root.
+#
 
 ## Fail if something went wrong
 set -euo pipefail
@@ -11,9 +18,9 @@ set -euo pipefail
 # Authorize sudo
 sudo echo
 
-GIT_PATH="$(which git)"
-
-if [[ -z GIT_PATH ]]; then
+# Install git if command "git" was not found
+if ! command -v git &> /dev/null 
+then
     sudo dnf install git
 fi
 
@@ -25,6 +32,7 @@ if [[ "$c_answer_awesome" == "y" ]]; then
     cp -r config/kitty ~/.config/
     cp -r config/picom ~/.config/
     
+    git clone https://github.com/demingongo/awesomewm-mpris-widget.git ~/.config/awesome/awesomewm-mpris-widget
     git clone https://github.com/streetturtle/awesome-wm-widgets.git ~/.config/awesome/awesome-wm-widgets
     git clone https://github.com/lcpz/awesome-freedesktop.git ~/.config/awesome/freedesktop
     git clone https://github.com/lcpz/lain.git ~/.config/awesome/lain
@@ -83,59 +91,11 @@ Exec=awesome
     
 fi
 
-echo
-echo -n "Copy dotfiles for neovim ? [y/N] "
-read c_answer_neovim
-if [[ "$c_answer_neovim" == "y" ]]; then
-    cp -r config/nvim ~/.config/
-fi
-
-c_answer_neovim_dep="y"
-if [[ "$c_answer_neovim" != "y" ]]; then
-    echo
-    echo -n "Install neovim and its dependencies ? [y/N] "
-    read c_answer_neovim_dep
-fi
-
-if [[ "$c_answer_neovim_dep" == "y" ]]; then
-    ## Neovim
-    #
-    # neovim, gcc-c++, go
-    #
-    sudo dnf install neovim gcc-c++ go
-    #
-    # vim-plug: https://github.com/junegunn/vim-plug
-    #
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    #
-    # ascii-image-converter
-    #
-    go install github.com/TheZoraiz/ascii-image-converter@latest
-    #
-    # node, npm
-    #
-    set +e
-    NPM_PATH="$(which npm)"
-    if [[ -z NPM_PATH ]]; then
-        NVM_PATH="$(which nvm)"
-        if [[ -z NVM_PATH ]]; then
-            # install nvm
-            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-        fi
-        # install node
-        nvm install node
-    fi
-    #
-    # lsp
-    #
-    npm i -g pyright @angular/language-server vscode-langservers-extracted typescript typescript-language-server yaml-language-server
-    set -e
-fi
-
 ### Fonts
 #
-# https://github.com/ryanoasis/nerd-fonts
+# - Hack font (see https://github.com/ryanoasis/nerd-fonts#option-3-install-script)
 #
-# Install Hack font (see https://github.com/ryanoasis/nerd-fonts#option-3-install-script)
+# - Vanilla Caramel (https://www.dafont.com/vanilla-caramel.font)
+#
+# - Purple Smile (https://www.dafont.com/purple-smile.font)
 #
